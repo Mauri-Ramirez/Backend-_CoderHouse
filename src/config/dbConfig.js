0fba80e0ba326566918b66572fa54d6674f7376a
Mauricio Ramirez <maurizio_514@hotmail.com>
2023-05-23 00:41:10 -0300

    correcion ruta chat

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -16,14 +16,3 @@ module.exports = {
     } 
 }
 
-//colocar el nombre del documento en el que queremos guardar despues de mongodb.net/ o se guardara por defecto en test
-//let url = "mongodb+srv://mauricioandres9308:uwQzdhQJeOQZwn3a@cluster0.jmtd4ap.mongodb.net/?retryWrites=true&w=majority"
-
-
-/* module.exports = {
-    connectDB: () =>{
-     connect(url)
-     console.log("base de datos conectada");
-    } 
-
-} */
--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -3,14 +3,14 @@ const messageModel = require("../dao/models/message.model")
 
 const router = Router()
 
-router.get("/", async (req, res)=>{
+/* router.get("/", async (req, res)=>{
     const messages = await messageModel.find().lean()
     res.render("chat", {messages})
 })  
-
- /* router.get("/", (req, res)=>{
+ */
+ router.get("/", (req, res)=>{
      res.render("chat", {})
- }) */ 
+ })
 
 
 
