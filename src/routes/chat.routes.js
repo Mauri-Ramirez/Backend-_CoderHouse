634ab23c2ae081144453322bda655bc6d3344662
Mauricio Ramirez <maurizio_514@hotmail.com>
2023-08-07 19:45:42 -0300

    jwt- chat

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express")
 const { authMiddleware } = require ("../middlewares/auth.middleware")
 const passportCall = require("../middlewares/passport.middleware")
+const { userMiddleware } = require("../middlewares/role.middleware.js")
 
 const router = Router()
 
  router.get("/",
- authMiddleware,
  passportCall("jwt"),
+ userMiddleware,
  (req, res)=>{
      res.render("chat", {})
  })
